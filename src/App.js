bbf4c409cc75bad41818f3ec840f87abb323fa0b
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-03-29 22:06:55 +0100

    Basic App Scaffold

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import "./assets/main.css";
 
 function App() {
   return (
-    <div>
-      <h1>App</h1>
+    <div className="bg-gray-200 pt-8 px-4 min-h-screen">
+      <h1 className="text-2xl sm:text-4xl font-bold text-gray-900 leading-tight">
+        Quiz from anywhere.
+        <span className="text-indigo-500"> Built Using React</span>
+      </h1>
     </div>
   );
 }
