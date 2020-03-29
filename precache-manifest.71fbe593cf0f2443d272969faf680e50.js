7cdee0da0226856f1dca7dce9906cd50422b4ff5
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-03-29 22:14:16 +0100

    Updates

---
+++ precache-manifest.71fbe593cf0f2443d272969faf680e50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c16af08c050bc3a7168bd8ced40629",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "70a881ef8c266c245066",
+    "url": "/react-quiz/static/css/main.119390ab.chunk.css"
+  },
+  {
+    "revision": "3a525bbef5a97ce9cb3e",
+    "url": "/react-quiz/static/js/2.a324754e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quiz/static/js/2.a324754e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a881ef8c266c245066",
+    "url": "/react-quiz/static/js/main.2b88a3ea.chunk.js"
+  },
+  {
+    "revision": "b1f2344ec01366e134c1",
+    "url": "/react-quiz/static/js/runtime-main.9e74c316.js"
+  }
+]);
