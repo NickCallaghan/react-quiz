908c2ad5b0ab5ba1a9c4b1fcaca502f5c9c9de16
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-03-30 20:36:31 +0100

    adding values to scorecard

--- src/components/ScoreCard/ScoreCard.js
+++ src/components/ScoreCard/ScoreCard.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+import { QuizSessionContext } from "../../contexts/QuizSessionContext";
+
 import Tile from "../Tile/Tile";
 
 export default function ScoreCard() {
+  // playerName: null,
+  // state: "NEW",
+  // totalQuestions: 0,
+  // questionsAnswered: 0,
+  // questionsCorrect: 0
+  const quizSession = useContext(QuizSessionContext);
+  const { questionsAnswered, questionsCorrect } = quizSession;
+
   return (
     <div className="flex justify-center pb-4 flex-col">
       <h1 className="text-4xl font-semibold text-center tracking-tight">
         Scorecard
       </h1>
       <div className="flex flex-col md:flex-row justify-center py-3">
-        <Tile heading="Total Questions" value={0}></Tile>
-        <Tile heading="Correct" value={0}></Tile>
-        <Tile heading="Incorrect" value={0}></Tile>
+        <Tile heading="Total Answered" value={questionsAnswered}></Tile>
+        <Tile heading="Correct" value={questionsCorrect}></Tile>
+        <Tile
+          heading="Incorrect"
+          value={questionsAnswered - questionsCorrect}
+        ></Tile>
       </div>
     </div>
   );
