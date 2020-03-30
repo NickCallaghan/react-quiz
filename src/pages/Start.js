5abb6ba810adb2e4a1edd8f54b4b6bfa6c569fb8
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-03-30 23:32:32 +0100

    Styling Start page and make name required

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -8,16 +8,30 @@ export default function Start(props) {
   const [input, setInput] = useState("");
 
   const handleStartQuiz = () => {
-    console.log("start");
-    dispatch({ type: "AddPlayerName", playerName: input });
-    props.history.push("/quiz");
+    if (input) {
+      dispatch({ type: "AddPlayerName", playerName: input });
+      props.history.push("/quiz");
+    }
   };
 
   return (
-    <div>
-      <InputText value={input} onChange={e => setInput(e.target.value)} />
-
-      <Button onClick={handleStartQuiz} label="Start" />
+    <div className="flex flex-col justify-center py-12 bg-indigo-300 min-h-screen">
+      <h1 className="text-center font-bold text-4xl text-gray-100 mb-12">
+        Welcome to{" "}
+        <span className="text-indigo-500 border-indigo-500 border-solid border-4 px-3 mr-2 ml-1 py-0 inline-block">
+          React
+        </span>
+        Quiz
+      </h1>
+      <form className="flex flex-col px-10">
+        <InputText
+          style={{ marginBottom: "1em" }}
+          placeholder="Enter you name"
+          value={input}
+          onChange={e => setInput(e.target.value)}
+        />
+        <Button onClick={handleStartQuiz} label="Start Quiz" type="submit" />
+      </form>
     </div>
   );
 }
