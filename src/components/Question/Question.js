87f777a482733ec84fcbdaef11544dda1ab1f94e
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-03-30 13:07:29 +0100

    Ability to answer question and update state

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "primereact/card";
-import { Button } from "primereact/button";
+import { RadioButton } from "primereact/radiobutton";
 
 export default function Question(props) {
-  const { questionText, answers } = props.question;
+  const { questionText, answers, correctAnswer } = props.question;
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [isCorrect, setIsCorrect] = useState(null);
 
   const header = (
     <div className="bg-indigo-700 text-xl white py-4 px-2 font-semibold uppercase text-white">
       {questionText}
     </div>
   );
+
+  const handleChooseAnswer = answerIndex => {
+    setSelectedAnswer(answerIndex);
+    correctAnswer.indexOf(answerIndex) >= 0
+      ? setIsCorrect(true)
+      : setIsCorrect(false);
+  };
 
   // const footer = (
   //   <span>
@@ -29,11 +38,23 @@ export default function Question(props) {
   return (
     <div className="mb-4">
       <Card header={header}>
-        <ol>
-          {answers.map(a => (
-            <li>{a}</li>
-          ))}
-        </ol>
+        {answers.map((a, index) => (
+          <div className="mb-2">
+            <RadioButton
+              checked={selectedAnswer === index ? true : false}
+              inputId={index}
+              name={`answer-${index}`}
+              value={index}
+              onChange={e => handleChooseAnswer(e.value)}
+            />
+            <label
+              htmlFor={`answer-${index}`}
+              className="p-radiobutton-label ml-4"
+            >
+              {a}
+            </label>
+          </div>
+        ))}
       </Card>
     </div>
   );
