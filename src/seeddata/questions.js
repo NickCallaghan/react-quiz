823c213ef8ec279279315225489047fe6c322580
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-03-30 14:10:45 +0100

    adding id to questions

--- src/seeddata/questions.js
+++ src/seeddata/questions.js
@@ -1,5 +1,8 @@
+import { v4 as uuid } from "uuid";
+
 const seedQuestions = [
   {
+    id: uuid(),
     questionText: "JSON name/value pair is written as",
     answers: [
       "name’ : ‘value’",
@@ -10,22 +13,26 @@ const seedQuestions = [
     correctAnswer: [3]
   },
   {
+    id: uuid(),
     questionText: "JSON strings have to be in",
     answers: ["single quote", "double quote", "single quote or double quote"],
     correctAnswer: [1]
   },
   {
+    id: uuid(),
     questionText:
       "In the below notation, Employee is of type { “Employee”: [ “Amy”, “Bob”, “John” ] }",
     answers: ["Not a valid JSON string", "Array", "Class", "Object"],
     correctAnswer: [1]
   },
   {
+    id: uuid(),
     questionText: "Which of the following is not a JSON type?",
     answers: ["Object", "date", "Array", "string"],
     correctAnswer: [1]
   },
   {
+    id: uuid(),
     questionText: "Which of these is correct about the JSON standard?",
     answers: [
       "It is an open standard",
@@ -35,6 +42,7 @@ const seedQuestions = [
     correctAnswer: [0]
   },
   {
+    id: uuid(),
     questionText: "What is the file extension of JSON?",
     answers: [".jn", ".js", ".jsn", ".json"],
     correctAnswer: [3]
